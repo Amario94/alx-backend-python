9b0694112a42f3f3d14164975346b1863217e38c
onebyte2016 <onebyte2016@gmail.com>
2024-06-06 10:29:39 -0700

    float n

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import math
+'''using function floor and math module
+'''
+def floor(n:float)->int:
+    '''geting a float value of n
+       with math function
+    '''
+    return math.floor(n)
