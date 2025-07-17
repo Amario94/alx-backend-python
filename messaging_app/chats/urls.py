9bdd4ed73ec37ee18c5bd236dee9215512c5c54b
onebyte2016 <onebyte2016@gmail.com>
2025-07-17 15:19:40 +0000

    django message app

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -21,8 +21,10 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_nested.routers import NestedDefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
+
 router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
+
 
 nested_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
 nested_router.register(r'messages', MessageViewSet, basename='conversation-messages')
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework import status, filters
 
 # Create your views here.
 from rest_framework import viewsets, permissions
