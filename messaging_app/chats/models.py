5bdf3d44e5be85b3fb511c7cf43a45a0c63069a0
onebyte2016 <onebyte2016@gmail.com>
2025-07-17 07:08:04 +0000

    django message app

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,22 +1,26 @@
 from django.db import models
-
-# Create your models here.
-
+import uuid
 from django.contrib.auth.models import AbstractUser
-from django.db import models
+
 
 class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
     name = models.CharField(max_length=50, blank=True, null=True)
     first_name = models.CharField(max_length=50, blank=True, null=True)
     last_name = models.CharField(max_length=50, blank=True,null=True)
-    phone = models.CharField(max_length=50, blank=True,null=True)
     address = models.CharField(max_length=50, blank=True,null=True)
     bio = models.CharField(max_length=50, blank=True,null=True)
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']  # username is still required if not removed
+
     def __str__(self):
-        return self.username
+        return f"{self.username} - {self.email}"
 
 class Conversation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     participants = models.ManyToManyField('User', related_name='conversations')
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -24,6 +28,7 @@ class Conversation(models.Model):
         return f"Conversation {self.pk}"
 
 class Message(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
     sender = models.ForeignKey('User', on_delete=models.CASCADE, related_name='sent_messages')
     content = models.TextField()
