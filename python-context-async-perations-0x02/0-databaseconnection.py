dc87411a4e83c77b01d1500a57afc7d0d3b6d15f
Anya Amarachi <anyaamarachi47@gmail.com>
2025-07-17 19:05:19 -0700

    Create 0-databaseconnection.py

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+class DatabaseConnection:
+    def __init__(self):
+        self.conn = None
+
+    def __enter__(self):
+        self.conn = sqlite3.connect("users.db")
+        return self.conn
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.conn:
+            self.conn.close()
+
+# ✅ Use the context manager with the WITH statement
+with DatabaseConnection() as conn:
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    results = cursor.fetchall()
+    print(results)
