29e2dda54e6c5ca3412a994c2e2fb7daeeafa995
onebyte2016 <onebyte2016@gmail.com>
2025-08-10 20:05:52 +0000

    dorker files

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -87,6 +87,20 @@ DATABASES = {
     }
 }
 
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE'),
+        'USER': os.environ.get('MYSQL_USER'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
+        'HOST': os.environ.get('MYSQL_HOST'),
+        'PORT': os.environ.get('MYSQL_PORT', 3306),
+    }
+}
+
+
 AUTH_USER_MODEL = 'chats.User'
 
 # Password validation
